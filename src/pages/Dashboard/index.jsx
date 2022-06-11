47e3708a45748cc4a36f52f45109786de88db934
Gustavo <56096596+gustavogamasa@users.noreply.github.com>
2022-06-11 17:16:27 -0300

    Dashboard - Primeiro container

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -1,33 +1,54 @@
-import { useContext } from 'react'
+import { useContext, useState } from 'react'
 import { AuthContext } from '../../contexts/auth'
+import { Link } from 'react-router-dom';
+
 import Header from '../../components/Header';
+import Title from '../../components/Title'
+import { FiMessageSquare, FiPlus } from 'react-icons/fi'
+import './dashboard.css'
 
 
 export default function Dashboard() {
 
     const { signOut } = useContext(AuthContext);
-
-    // function mensagemBemVindo(){
-    //     toast.success("Bem vindo de volta!");
-    // }
-    
-
-
-
+    const [chamados, setChamados] = useState([]);
 
 
     return (
         <div>
-           
-            <Header></Header>
-            Dashboard <h2>LOGADO</h2>
-            <button onClick={() => signOut()}> Fazer logout </button>
+
+            <Header />
+            <div className="content">
+                <Title name="Atendimentos">
+                    <FiMessageSquare size={25}></FiMessageSquare>
+                </Title>
+
+                {chamados.length === 0 ? (<div className="container dashboard">
+
+                    <span>Nenhum chamado registrado...</span>
+                    <Link to="/new" className='new'>
+                        <FiPlus size={25} color="#FFF" /> Novo chamado
+                    </Link>
+
+                </div>
+
+                    //IF SEM CONTEÚDO
+
+                ) : (
+
+                    <>
+                        <Link to="/new" className='new'>
+                            <FiPlus size={25} color="#FFF" /> Novo chamado
+                        </Link>
+                    </>
+                )}
 
 
 
 
 
 
+            </div>
 
         </div >
     )
