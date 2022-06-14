6672d8750f6c0296ec72d569ff31c19ad0026ee8
Gustavo <56096596+gustavogamasa@users.noreply.github.com>
2022-06-14 13:45:30 -0300

    Dashboard - Table ok

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -4,14 +4,14 @@ import { Link } from 'react-router-dom';
 
 import Header from '../../components/Header';
 import Title from '../../components/Title'
-import { FiMessageSquare, FiPlus } from 'react-icons/fi'
+import { FiMessageSquare, FiPlus, FiSearch, FiEdit2 } from 'react-icons/fi'
 import './dashboard.css'
 
 
 export default function Dashboard() {
 
     const { signOut } = useContext(AuthContext);
-    const [chamados, setChamados] = useState([]);
+    const [chamados, setChamados] = useState([1]);
 
 
     return (
@@ -32,7 +32,7 @@ export default function Dashboard() {
 
                 </div>
 
-                    //IF SEM CONTEÚDO
+                    // IF SEM CONTEÚDO
 
                 ) : (
 
@@ -40,8 +40,40 @@ export default function Dashboard() {
                         <Link to="/new" className='new'>
                             <FiPlus size={25} color="#FFF" /> Novo chamado
                         </Link>
+
+                        {/* TABELA */}
+                        <table>
+                            <thead>
+                                <tr>
+                                    <th scope='col'>Cliente</th>
+                                    <th scope='col'>Assunto</th>
+                                    <th scope='col'>Status</th>
+                                    <th scope='col'>Cadastrado em</th>
+                                    <th scope='col'>#</th>
+                                </tr>
+                            </thead>
+
+                            <tbody>
+                                <tr>
+                                    <td data-label='Cliente'>Sujeito</td>
+                                    <td data-label='Assunto'>Suporte</td>
+                                    <td data-label='Status'>
+                                        <span className='badge' style={{ backgroundColor: "#5cb85c" }}>Em aberto</span>
+                                    </td>
+                                    <td data-label='Cadastrado'>20/06/2022</td>
+                                    <td data-label='#'>
+                                        <button className='action' style={{ backgroundColor: "#3583f6" }}><FiSearch color='#FFF' size={17} /></button>
+                                        <button className='action' style={{ backgroundColor: "#F6a935" }}><FiEdit2 color='#FFF' size={17} /></button>
+
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+
                     </>
                 )}
+
+                {/* FIM SCRIPT */}
 
 
 
